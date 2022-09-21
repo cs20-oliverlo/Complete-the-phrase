fa655f37c495a408ec52924bcea5b8930f2c0a6b
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-09-21 10:54:55 -0600

    awd

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 // JavaScript Code
+
+document.getElementById("btn1").addEventListener("click", f1);
+
+function f1() {
+    alert("it.");
+}
+
+document.getElementById("btn2").addEventListener("click", f2);
+
+function f2() {
+    alert("sack/hay.");
+}
+
+document.getElementById("btn3").addEventListener("click", f3);
+
+function f3() {
+    alert("there.");
+}
+
+document.getElementById("btn4").addEventListener("click", f4);
+
+function f4() {
+    alert("weather.");
+}
+
+document.getElementById("btn5").addEventListener("click", f5);
+
+function f5() {
+    alert("that again.");
+}
